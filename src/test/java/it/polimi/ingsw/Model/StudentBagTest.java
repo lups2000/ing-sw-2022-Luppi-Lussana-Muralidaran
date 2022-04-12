491f4c11c27f7f642adeb0af581930d2017ed044
DarthPrader00 <pradeeban.muralidaran@mail.polimi.it>
2022-04-12 16:36:06 +0200

    Creation of the various tests for each class

--- src/test/java/it/polimi/ingsw/Model/StudentBagTest.java
+++ src/test/java/it/polimi/ingsw/Model/StudentBagTest.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.Model;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,18 +8,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class StudentBagTest {
 
     @Test
+    @DisplayName("getStudents")
     void getStudents() {
     }
 
     @Test
+    @DisplayName("getNumStudents")
     void getNumStudents() {
     }
 
     @Test
+    @DisplayName("addStudents")
     void addStudents() {
     }
 
     @Test
+    @DisplayName("drawStudent")
     void drawStudent() {
     }
 }
