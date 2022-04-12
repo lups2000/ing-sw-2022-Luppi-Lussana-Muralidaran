6eee7d8899128ed1f7d9d15abe89e0fb01f7923d
DarthPrader00 <pradeeban.muralidaran@mail.polimi.it>
2022-04-12 16:42:14 +0200

    Creation of the various tests for each class

---
+++ src/test/java/it/polimi/ingsw/Model/AssistantCardTest.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.Model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * AssistantCard class is tested by AssistantCardTest
+ */
+
+class AssistantCardTest {
+
+    @Test
+    @DisplayName("getValue")
+    void getValue() {
+    }
+
+    @Test
+    @DisplayName("getMaxStepsMotherNature")
+    void getMaxStepsMotherNature() {
+    }
+}
