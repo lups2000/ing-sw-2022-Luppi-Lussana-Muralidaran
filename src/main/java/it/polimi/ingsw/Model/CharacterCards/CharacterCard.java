e8154032214342e5aa3a7c28f3f778155cc2776f
lups2000 <matteo1.luppi@mail.polimi.it>
2022-04-13 13:35:48 +0200

    add few comments for us

--- src/main/java/it/polimi/ingsw/Model/CharacterCards/CharacterCard.java
+++ src/main/java/it/polimi/ingsw/Model/CharacterCards/CharacterCard.java
@@ -2,10 +2,11 @@ package it.polimi.ingsw.Model.CharacterCards;
 
 public abstract class CharacterCard {
     private int cost;
+    private boolean used;
 
-    public int getCost() {
-        return cost;
-    }
+    public int getCost() {return cost;}
+    public boolean isUsed() {return used;}
+
     public void effect(){
 
     }
--- src/main/java/it/polimi/ingsw/Model/DeckCharacterCard.java
+++ src/main/java/it/polimi/ingsw/Model/DeckCharacterCard.java
@@ -1,8 +1,22 @@
 package it.polimi.ingsw.Model;
 
+import it.polimi.ingsw.Model.CharacterCards.CharacterCard;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DeckCharacterCard {
+    List<CharacterCard> characterCards;
 
     public DeckCharacterCard(){
+        this.characterCards=new ArrayList<>();
+        //qua dobbiamo aggiungere al mazzo tutte e 12 le carte
+        //da fare dopo quando abbiamo definito tutti le 12 classi
+    }
+
+    public List<CharacterCard> pickThreeCards(){
+        //method to extract randomly the three character cards
         //TODO
+        return null;
     }
 }
