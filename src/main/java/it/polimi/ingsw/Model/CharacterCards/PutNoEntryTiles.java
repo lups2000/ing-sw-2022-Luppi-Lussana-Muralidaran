f1315455bf6d9bc852fc7718b50b3c388964ae63
DarthPrader00 <pradeeban.muralidaran@mail.polimi.it>
2022-04-22 12:17:55 +0200

    - Created an Exception if the number of No Entry Tiles becomes negative;

--- src/main/java/it/polimi/ingsw/Model/CharacterCards/PutNoEntryTiles.java
+++ src/main/java/it/polimi/ingsw/Model/CharacterCards/PutNoEntryTiles.java
@@ -2,16 +2,18 @@ package it.polimi.ingsw.Model.CharacterCards;
 
 import it.polimi.ingsw.Model.Island;
 import it.polimi.ingsw.Model.Game;
-import it.polimi.ingsw.Model.Exceptions.NoNoEntryTilesAvailableException;
+import it.polimi.ingsw.Model.Exceptions.NoNoEntryTilesException;
 
 /**
  * @author Pradeeban Muralidaran
  */
 
-//In setup, put the 4 no entry tiles on this card.
-//EFF: place a no entry tile on an island of your choice.
-//  The first time mother nature ends her movement there, put the no entry tile back onto this card
-//  DO NOT calculate influence on that island, or place any towers
+/*
+In setup, put the 4 no entry tiles on this card.
+EFF: place a no entry tile on an island of your choice.
+The first time mother nature ends her movement there, put the no entry tile back onto this card
+DO NOT calculate influence on that island, or place any towers
+*/
 
 public class PutNoEntryTiles extends CharacterCard {
 
@@ -21,12 +23,12 @@ public class PutNoEntryTiles extends CharacterCard {
         this.game = game;
     }
 
-    public void effect(Island island) throws NoNoEntryTilesAvailableException{
+    public void effect(Island island) throws NoNoEntryTilesException{
         if (game.getNoEntryTilesCounter() > 0){
             island.setNoEntryTiles(1);
             game.setNoEntryTilesCounter(game.getNoEntryTilesCounter()-1);
             used();
         }
-        else {throw new NoNoEntryTilesAvailableException();}
+        else {throw new NoNoEntryTilesException();}
     }
 }
--- src/main/java/it/polimi/ingsw/Model/Exceptions/NoNoEntryTilesAvailableException.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.Model.Exceptions;
-
-public class NoNoEntryTilesAvailableException extends Exception{
-
-    @Override
-    public String getMessage() {
-        return ("There are no available tiles at the moment!");
-    }
-}
