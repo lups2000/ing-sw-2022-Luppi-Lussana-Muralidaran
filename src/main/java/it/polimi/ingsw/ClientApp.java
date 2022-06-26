7a093c63cdc0890814e18ead6c7355ac75495e6b
Matteo <matteo1.luppi@mail.polimi.it>
2022-06-26 15:12:20 +0200

    fix pom for jars

--- src/main/java/it/polimi/ingsw/ClientApp.java
+++ src/main/java/it/polimi/ingsw/ClientApp.java
@@ -5,27 +5,30 @@ import it.polimi.ingsw.View.Cli.Cli;
 import it.polimi.ingsw.View.Gui.GuiMainStage;
 import javafx.application.Application;
 
+import java.util.Scanner;
+
 public class ClientApp {
 
     public static void main(String[] args) {
 
-        boolean flag=false;
 
-        for(String arg : args){
-            if(arg.equals("--cli")){
-                flag=true;
-                break;
-            }
-        }
+        System.out.println("Choose the modality: 'c' for CLI / 'g' for GUI");
+        Scanner scanner = new Scanner(System.in);
+        String answer;
+        answer=scanner.nextLine();
 
-        if(flag){
+        if(answer.equalsIgnoreCase("c")){
             Cli view = new Cli();
             ClientController clientcontroller = new ClientController(view);
             view.addObserver(clientcontroller);
             view.matchStart();
         }
-        else{
+        else if(answer.equalsIgnoreCase("g")){
             Application.launch(GuiMainStage.class);
         }
+        else{
+            System.err.println("Invalid Argument! Please run the executable again!");
+        }
+
     }
 }
