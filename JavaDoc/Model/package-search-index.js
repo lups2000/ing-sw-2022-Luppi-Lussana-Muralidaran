5d70ca882b346b684b30ba2861871505909e7996
Matteo <matteo1.luppi@mail.polimi.it>
2022-06-29 18:01:33 +0200

    javaDoc created for Model

---
+++ JavaDoc/Model/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"it.polimi.ingsw.Model"},{"l":"it.polimi.ingsw.Model.CharacterCards"},{"l":"it.polimi.ingsw.Model.Exceptions"}];updateSearchResults();
